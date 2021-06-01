faa12873f727767a22dc1fb0f7d093cb0c83ae5d
polevault7 <whitevol@mail.ru>
2021-06-01 10:28:24 +0300

    last commit

--- src/main/java/com/example/roomchecker/model/User.java
+++ src/main/java/com/example/roomchecker/model/User.java
@@ -1,8 +1,8 @@
 package com.example.roomchecker.model;
 
-import java.util.Objects;
+import lombok.Data;
 
-
+@Data
 public class User {
     private long id;
     private String name;
@@ -14,60 +14,5 @@ public class User {
         this.name = name;
         this.isInside = isInside;
         this.room = room;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean isInside() {
-        return isInside;
-    }
-
-    public Room getRoom() {
-        return room;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setInside(boolean inside) {
-        isInside = inside;
-    }
-
-    public void setRoom(Room room) {
-        this.room = room;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && isInside == user.isInside && Objects.equals(name, user.name) && Objects.equals(room, user.room);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, isInside, room);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", isInside=" + isInside +
-                ", room=" + room +
-                '}';
     }
 }
